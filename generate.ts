08a62014a787738b32cade2ab5017c454efa7a91
Richard Haddad <richardhaddad@hotmail.fr>
2026-04-10 20:43:07 +0200

    chore: use remote schema

--- generate.ts
+++ generate.ts
@@ -2,15 +2,22 @@ import fs from 'node:fs';
 import type { JSONSchema4 } from 'json-schema';
 import { compile } from 'json-schema-to-typescript';
 
-const fileContent: JSONSchema4 = JSON.parse(
-  fs.readFileSync('config.schema.json', 'utf8'),
-);
+const schemaUrl =
+  'https://raw.githubusercontent.com/Chnapy/gatus/refs/heads/feat/add-jsonschema-generation/config.schema.json';
 
-// root $ref requires to be undefined
-// @see https://github.com/bcherny/json-schema-to-typescript/issues/681
-delete fileContent.$ref;
+fetch(schemaUrl)
+  .then((res) => res.json() as Promise<JSONSchema4>)
+  .then((fileContent) => {
+    // root $ref requires to be undefined
+    // @see https://github.com/bcherny/json-schema-to-typescript/issues/681
+    delete fileContent.$ref;
 
-compile(fileContent, 'GatusConfig', {
-  unreachableDefinitions: true,
-  format: false,
-}).then((ts) => fs.writeFileSync('index.d.ts', ts));
+    return fileContent;
+  })
+  .then((fileContent) =>
+    compile(fileContent, 'GatusConfig', {
+      unreachableDefinitions: true,
+      format: false,
+    }),
+  )
+  .then((ts) => fs.writeFileSync('index.d.ts', ts));
